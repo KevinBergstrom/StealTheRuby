341b1f270e2b7eb0312e0f55a62f729728a074bf
kevbergstrom <kevin.bergstrom@wsu.edu>
2018-10-12 20:34:02 -0700

    Completed issue23: Added fruit item that spawns a fruit peel

---
+++ stealTheRuby/src/stealTheRuby/fruit.java
@@ -0,0 +1,35 @@
+package stealTheRuby;
+
+import org.newdawn.slick.state.StateBasedGame;
+
+import jig.Vector;
+
+public class fruit extends Item{
+
+	final int type = 3;
+	
+	public fruit(float x, float y, boolean sol) {
+		super(x, y, sol);
+		setImage(Trap.PEELIMG_RSC,32,32);
+	}
+	
+	@Override
+	public void pickup(StateBasedGame game) {
+		MainGame mg = (MainGame)game;
+		mg.player.addItem(this);
+		
+		addCollectAnim(getX(),getY(),400,520,type,game);
+	
+		removeThis(game);
+	}
+	
+	@Override
+	public void use(StateBasedGame game) {
+		MainGame mg = (MainGame)game;
+		Vector gridPos = mg.map.getGridPos(mg.player.getX(), mg.player.getY());
+		fruitPeel peel = new fruitPeel(gridPos.getX()*mg.map.getTileSizeX() + mg.map.getTileSizeX()/2,gridPos.getY()*mg.map.getTileSizeY() + mg.map.getTileSizeY()/2,true);
+		mg.map.setTrap((int)gridPos.getX(), (int)gridPos.getY(), peel);
+		mg.player.removeItem();
+	}
+
+}
